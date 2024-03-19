c9053fc2192a871e9bb6164c547c95b7ed7d7a4e
Chen Jiale <137020777+bbbbeta@users.noreply.github.com>
2024-03-20 01:06:26 +0800

    Add files via upload

---
+++ assignment3/Homework3_2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace Homework3_2
+{
+    public static class ShapeFactory
+    {
+        public static Shape Create(String typeOfShape) {
+            Random random = new Random();
+            switch(typeOfShape)
+            {
+                case "Rectangle":
+                    return new Rectangle { length = random.Next(1, 10), width = random.Next(1, 10) };
+                case "Square":
+                    return new Square { length = random.Next(1, 10) };
+                case "Triangle":
+                    return new Triangle { baselength = random.Next(1, 10) ,height=random.Next(1,10)};
+                default:
+                    throw new ArgumentException("类型错误");
+            }
+        }
+
+    }
+
+    public interface Shape
+    {
+        double GetArea();
+    }
+
+    // 长方形类
+    public class Rectangle : Shape
+    {
+        public double length { get; set; }
+        public double width { get; set; }
+        public double GetArea() => length * width;
+    }
+
+    // 正方形类
+    public class Square : Shape
+    {
+        public double length { get; set; }
+        public double GetArea() => length * length;
+    }
+
+    // 三角形类
+    public class Triangle : Shape
+    {
+        public double baselength { get; set; }
+        public double height { get; set; }
+        public double GetArea() => 0.5 * baselength * height;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double areaSum = 0;
+            string[] shapeTypes = { "Rectangle", "Square", "Triangle" };
+            for (int i = 0; i < 10; i++)
+            {
+                string typeOfShape = shapeTypes[new Random().Next(0, shapeTypes.Length)];
+                Shape shape = ShapeFactory.Create(typeOfShape);
+                areaSum += shape.GetArea();
+            }
+
+            Console.WriteLine($"10个形状的总面积为: {areaSum}");
+        }
+    }
+}
---
+++ assignment3/homework3_1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Homework3_1
+{
+    //interface
+    public interface Shape
+    {
+        double GetArea();//计算面积
+        Boolean IsLegal();//合法性检测
+    }
+
+    //abstract
+    public abstract class ShapeAbstract : Shape
+    {
+        
+        public abstract double GetArea();
+        public abstract bool IsLegal();
+    }
+
+    public class Rectangle : Shape
+    {
+        private double length { get; set; }
+        private double width { get; set; }
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double GetArea() => length * width;
+        public bool IsLegal() => (length >= 0 && width >= 0);
+        
+    }
+
+    public class Square : ShapeAbstract
+    {
+        private double length { get; set; }
+
+        public Square(double length)
+        {
+            this.length = length;
+        }
+
+        public override double GetArea() => length * length;
+        public override bool IsLegal() => (length >= 0);
+    }
+
+    public class Triangle : Shape
+    {
+        private double baseLength {get;set; }
+        private double height { get; set; }
+
+        public Triangle(double baseLength,double height)
+        {
+            this.baseLength = baseLength;
+            this.height = height;
+        }
+        public double GetArea()=>baseLength * height * 0.5;
+        public bool IsLegal() => (baseLength >= 0 && height >= 0);
+
+
+    }
+
+    public class Circle : ShapeAbstract
+    {
+        private double radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()=>radius * radius * 0.5;
+        public override bool IsLegal() => (radius >= 0);
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5, 10);
+            Console.WriteLine(rectangle.IsLegal() + " " + rectangle.GetArea());
+            Square square = new Square(10);
+            Console.WriteLine(square.IsLegal() + " " + square.GetArea());
+            Triangle triangle = new Triangle(6, 8);
+            Console.WriteLine(triangle.IsLegal() + " " + triangle.GetArea());
+            Circle circle = new Circle(2.5);
+            Console.WriteLine(circle.IsLegal() + " " + circle.GetArea());
+        }
+    }
+}
